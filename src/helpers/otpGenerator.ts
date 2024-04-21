03ea1dea3b94e6c07e22c14e2a21bad1f5492c46
rohitcodism <rohitpaula18i@gmail.com>
2024-04-21 10:32:40 +0530

    ✅ Add hashPassword and otpGenerator helper functions

---
+++ src/helpers/otpGenerator.ts
@@ -0,0 +1,8 @@
+function generateOtp(length = 6) {
+    const digits = '0123456789';
+    let otp = '';
+    for (let i = 0; i < length; i++) {
+        otp += digits.charAt(Math.floor(Math.random() * digits.length));
+    }
+    return otp;
+}
